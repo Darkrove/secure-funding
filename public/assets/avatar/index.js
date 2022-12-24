8f10b266dd3ec76cccfed18c09ac5c00ec2e397a
Sajjad Shaikh <samaralishaikh212@gmail.com>
2022-12-24 10:54:12 +0530

    tap-color

--- public/assets/avatar/index.js
+++ public/assets/avatar/index.js
@@ -13,7 +13,22 @@ import img12 from "./12.png";
 import img13 from "./13.png";
 import img14 from "./14.png";
 import img15 from "./15.png";
+import img16 from "./16.png";
+import img17 from "./17.png";
+import img18 from "./18.png";
+import img19 from "./19.png";
+import img20 from "./20.png";
+import img21 from "./21.png";
+import img22 from "./22.png";
+import img23 from "./23.png";
+import img24 from "./24.png";
+import img25 from "./25.png";
+import img26 from "./26.png";
+import img27 from "./27.png";
+import img28 from "./28.png";
+import img29 from "./29.png";
+import img30 from "./30.png";
 
-const images = [img1, img2, img3, img4, img5, img6, img7, img8, img9, img10, img11, img12, img13, img14, img15];
+const images = [img1, img2, img3, img4, img5, img6, img7, img8, img9, img10, img11, img12, img13, img14, img15, img16, img17, img18, img19, img20, img21, img22, img23, img24, img25, img26, img27, img28, img29, img30];
 
 export default images;
